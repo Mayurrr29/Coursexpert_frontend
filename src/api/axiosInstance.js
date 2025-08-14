b24ae8ef5e6687cbaec96f781d141ff3a641d4f3
Mayur <mp9099644464@gmail.com>
2025-08-14 12:55:40 +0530

    changed backend url in axios instance and socket js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://coursexpert-backend.up.railway.app", // <-- change here
+  baseURL: "coursexpertbackend-production.up.railway.app", // <-- change here
 });
 
 axiosInstance.interceptors.request.use(
