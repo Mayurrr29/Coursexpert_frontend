e23ff4467029ae18ab52d03c58d920983a03a652
Mayur <mp9099644464@gmail.com>
2025-08-14 12:58:18 +0530

    changed backend url in axios instance and socket js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://coursexpert-backend.up.railway.app'); // Your backend
+const socket = io('coursexpertbackend-production.up.railway.app'); // Your backend
 
 export default socket;
